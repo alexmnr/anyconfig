6951d96c2c45c3d8f2dcda06d49917c2db920b90
Senaxius <alexander.minor004@gmail.com>
2023-09-17 13:42:05 -0700

    install script update

--- install.sh
+++ install.sh
@@ -1,32 +1,201 @@
+# #!/bin/bash
+
+# if command -v pacman &> /dev/null; then
+#   os="arch"
+#   echo "INFO: Detected arch based system"
+# elif command -v apt &> /dev/null; then
+#   os="debian"
+#   echo "INFO: Detected debian based system"
+# else
+#   echo "ERROR: OS not supported"
+#   exit 1
+# fi
+
+# if [ ! -z "$(lscpu | grep 'aarch64')" ]; then
+#   arc="aarch64"
+#   echo "INFO: Detected aarch64 architecture"
+# elif [ ! -z "$(lscpu | grep 'armv7l')" ]; then
+#   arc="aarch64"
+#   echo "INFO: Detected aarch64 architecture"
+# elif [ ! -z "$(lscpu | grep 'x86_64')" ]; then
+#   arc="x86_64"
+#   echo "INFO: Detected x86_64 architecture"
+# else
+#   echo "ERROR: architecture not detected"
+#   exit 1
+# fi
+
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# if [ "$SCRIPT_DIR" = "/opt/anyconfig" ]; then
+#   sudo chown $USER:$USER /opt/anyconfig -R
+#   cd /opt/anyconfig
+# else
+#   sudo rm -rf /opt/anyconfig &>/dev/null
+#   sudo cp -r $SCRIPT_DIR /opt/anyconfig
+#   sudo chown $USER:$USER /opt/anyconfig -R
+#   cd /opt/anyconfig
+# fi
+
+# # check if dependencies are met
+# dep=true
+# if ! command -v git &> /dev/null; then
+#   dep=false
+# fi
+# if ! command -v go &> /dev/null; then
+#   dep=false
+# fi
+# if ! command -v wget &> /dev/null; then
+#   dep=false
+# fi
+# if ! command -v curl &> /dev/null; then
+#   dep=false
+# fi
+
+# if [ "$dep" = "false" ]; then
+#   # arch install
+#   if [ "$os" = "arch" ]; then
+#     if [ "$arc" = "x86_64" ]; then
+#       sudo pacman -Sy curl wget go git --needed --noconfirm
+#     else
+#       echo "ERROR: anyconfig can't automatically install dependencies on your system, you need to do it manually"
+#       exit 1
+#     fi
+#   # debian install
+#   elif [ "$os" = "debian" ]; then
+#     if [ "$arc" = "x86_64" ]; then
+#       sudo apt update && sudo apt install curl wget git -y
+#       sudo rm -rf /usr/local/go &> /dev/null
+#       cd /tmp
+#       wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz
+#       sudo tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
+#       if [ -z "$(echo $PATH | grep '/usr/local/go/bin')" ]; then
+#         echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/profile
+#         echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/zsh/zshenv
+#         PATH="$PATH:/usr/local/go/bin"
+#         export PATH
+#       fi
+#     elif [ "$arc" = "aarch64" ]; then
+#       sudo apt update && sudo apt install curl wget git -y
+#       sudo rm -rf /usr/local/go &> /dev/null
+#       cd /tmp
+#       wget https://go.dev/dl/go1.20.6.linux-armv6l.tar.gz
+#       sudo tar -C /usr/local -xzf go1.20.6.linux-armv6l.tar.gz
+#       if [ -z "$(echo $PATH | grep '/usr/local/go/bin')" ]; then
+#         echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/profile
+#         echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/zsh/zshenv
+#         PATH="$PATH:/usr/local/go/bin"
+#         export PATH
+#       fi
+#     else
+#       echo "ERROR: anyconfig can't automatically install dependencies on your system, you need to do it manually"
+#       exit 1
+#     fi
+#   fi
+# else
+#   echo "INFO: All Dependencies found"
+# fi
+
+# cd /opt/anyconfig/go
+# go build .
+
+# sudo rm -f /usr/bin/anyconfig &>/dev/null
+# sudo ln -s /opt/anyconfig/go/anyconfig /usr/bin
+
+# # install update service
+# sudo ln -s /opt/anyconfig/etc/anyconfig-update.service /usr/lib/systemd/user/
+# systemctl --user daemon-reload
+# systemctl --user enable anyconfig-update
+# systemctl --user start anyconfig-update
+
+# echo ""
+# echo "INFO: Installation complete"
+
 #!/bin/bash
-
-if command -v pacman &> /dev/null; then
-  os="arch"
-  echo "INFO: Detected arch based system"
-elif command -v apt &> /dev/null; then
-  os="debian"
-  echo "INFO: Detected debian based system"
-else
-  echo "ERROR: OS not supported"
-  exit 1
+# check if dependencies are met
+version="1.21.1"
+dep=true
+missing=""
+if ! command -v git &> /dev/null; then
+  dep=false
+  missing=$(echo "git $missing")
+fi
+if ! command -v wget &> /dev/null; then
+  dep=false
+  missing=$(echo "wget $missing")
+fi
+if ! command -v curl &> /dev/null; then
+  dep=false
+  missing=$(echo "curl $missing")
+fi
+if ! command -v sudo &> /dev/null; then
+  dep=false
+  missing=$(echo "sudo $missing")
 fi
 
-if [ ! -z "$(lscpu | grep 'aarch64')" ]; then
-  arc="aarch64"
-  echo "INFO: Detected aarch64 architecture"
-elif [ ! -z "$(lscpu | grep 'armv7l')" ]; then
-  arc="aarch64"
-  echo "INFO: Detected aarch64 architecture"
-elif [ ! -z "$(lscpu | grep 'x86_64')" ]; then
-  arc="x86_64"
-  echo "INFO: Detected x86_64 architecture"
-else
-  echo "ERROR: architecture not detected"
-  exit 1
+if [ "$dep" = "false" ]; then
+  echo "INFO: Not all dependencies were met, please install following packages: $missing"
+  exit
 fi
 
+# check if go is installed
+if ! command -v go &> /dev/null; then
+  go=false
+  echo "INFO: go not found, trying to install..."
+else
+  go=true
+fi
+
+# install go if necessary
+if [ "$go" = "false" ]; then
+  # get info about cpu
+  if [ ! -z "$(lscpu | grep 'aarch64')" ]; then
+    arc="aarch64"
+    echo "INFO: Detected aarch64 architecture"
+  elif [ ! -z "$(lscpu | grep 'armv7l')" ]; then
+    arc="aarch64"
+    echo "INFO: Detected aarch64 architecture"
+  elif [ ! -z "$(lscpu | grep 'x86_64')" ]; then
+    arc="x86_64"
+    echo "INFO: Detected x86_64 architecture"
+  else
+    echo "ERROR: architecture not detected"
+    exit 1
+  fi
+
+  # install go from source
+  if [ "$arc" = "x86_64" ]; then
+    sudo rm -rf /usr/local/go &> /dev/null
+    cd /tmp
+    link="https://go.dev/dl/go$version.linux-amd64.tar.gz"
+    wget $link
+    sudo tar -C /usr/local -xzf go$version.linux-amd64.tar.gz
+    sudo rm -f /usr/bin/go
+    sudo rm -f /usr/bin/gofmt
+    sudo ln -s /usr/local/go/bin/go /usr/bin
+    sudo ln -s /usr/local/go/bin/gofmt /usr/bin
+  elif [ "$arc" = "aarch64" ]; then
+    sudo rm -rf /usr/local/go &> /dev/null
+    cd /tmp
+    link="https://go.dev/dl/go$version.linux-armv6l.tar.gz"
+    wget $link
+    sudo tar -C /usr/local -xzf go$version.linux-armv6l.tar.gz
+    sudo rm -f /usr/bin/go
+    sudo rm -f /usr/bin/gofmt
+    sudo ln -s /usr/local/go/bin/go /usr/bin
+    sudo ln -s /usr/local/go/bin/gofmt /usr/bin
+  else
+    echo "ERROR: Can't automatically install go on your system, you need to do it manually"
+    exit 1
+  fi
+
+fi
+
+echo "INFO: All dependencies were met!"
+
+# move current dir to /opt/anyconfig
+echo "INFO: Installing anyconfig in /opt/anyconfig..."
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 if [ "$SCRIPT_DIR" = "/opt/anyconfig" ]; then
   sudo chown $USER:$USER /opt/anyconfig -R
   cd /opt/anyconfig
@@ -37,64 +206,7 @@ else
   cd /opt/anyconfig
 fi
 
-# check if dependencies are met
-dep=true
-if ! command -v git &> /dev/null; then
-  dep=false
-fi
-if ! command -v go &> /dev/null; then
-  dep=false
-fi
-if ! command -v wget &> /dev/null; then
-  dep=false
-fi
-if ! command -v curl &> /dev/null; then
-  dep=false
-fi
-
-if [ "$dep" = "false" ]; then
-  # arch install
-  if [ "$os" = "arch" ]; then
-    if [ "$arc" = "x86_64" ]; then
-      sudo pacman -Sy curl wget go git --needed --noconfirm
-    else
-      echo "ERROR: anyconfig can't automatically install dependencies on your system, you need to do it manually"
-      exit 1
-    fi
-  # debian install
-  elif [ "$os" = "debian" ]; then
-    if [ "$arc" = "x86_64" ]; then
-      sudo apt update && sudo apt install curl wget git -y
-      sudo rm -rf /usr/local/go &> /dev/null
-      cd /tmp
-      wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz
-      sudo tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
-      if [ -z "$(echo $PATH | grep '/usr/local/go/bin')" ]; then
-        echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/profile
-        echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/zsh/zshenv
-        PATH="$PATH:/usr/local/go/bin"
-        export PATH
-      fi
-    elif [ "$arc" = "aarch64" ]; then
-      sudo apt update && sudo apt install curl wget git -y
-      sudo rm -rf /usr/local/go &> /dev/null
-      cd /tmp
-      wget https://go.dev/dl/go1.20.6.linux-armv6l.tar.gz
-      sudo tar -C /usr/local -xzf go1.20.6.linux-armv6l.tar.gz
-      if [ -z "$(echo $PATH | grep '/usr/local/go/bin')" ]; then
-        echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/profile
-        echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/zsh/zshenv
-        PATH="$PATH:/usr/local/go/bin"
-        export PATH
-      fi
-    else
-      echo "ERROR: anyconfig can't automatically install dependencies on your system, you need to do it manually"
-      exit 1
-    fi
-  fi
-else
-  echo "INFO: All Dependencies found"
-fi
+echo "INFO: Building anyconfig..."
 
 cd /opt/anyconfig/go
 go build .
@@ -102,11 +214,10 @@ go build .
 sudo rm -f /usr/bin/anyconfig &>/dev/null
 sudo ln -s /opt/anyconfig/go/anyconfig /usr/bin
 
-# install update service
+echo "INFO: Linking anyconfig-update service..."
 sudo ln -s /opt/anyconfig/etc/anyconfig-update.service /usr/lib/systemd/user/
 systemctl --user daemon-reload
 systemctl --user enable anyconfig-update
 systemctl --user start anyconfig-update
 
-echo ""
-echo "INFO: Installation complete"
+echo "INFO: done!"
